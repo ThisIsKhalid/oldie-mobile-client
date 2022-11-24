a3a22734cfc15af689a4caf816dba21e9cb6706f
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-11-24 12:55:08 +0600

    added banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React from "react";
+import phoneSell from "../../../Assets/phone sell.svg";
+import phoneBuy from "../../../Assets/phone-buy.svg";
 
 const Banner = () => {
-    return (
-        <div>
-            <h1>this is banner</h1>
+  return (
+    <div className="carousel w-full">
+      <div id="slide1" className="carousel-item relative w-full">
+        <div className="bg-gradient-to-r from-secondary to-primary  w-full flex md:flex-row flex-col-reverse justify-evenly items-center px-5">
+          <div className="lg:w-1/2">
+            <h1 className=" text-4xl text-white font-medium">
+              Sell your{" "}
+              <span className="text-accent uppercase font-bold">phones</span>{" "}
+              from anywhere.
+            </h1>
+          </div>
+          <img className="h-96" src={phoneSell} alt="/" />
         </div>
-    );
+
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" className="btn btn-sm btn-circle btn-outline">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-sm btn-circle btn-outline">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <div className="bg-gradient-to-r from-accent to-neutral  w-full flex md:flex-row flex-col-reverse justify-evenly items-center px-5">
+          <div className="lg:w-1/2">
+            <h1 className=" text-4xl font-medium">
+              Buy <span className="text uppercase font-bold text-white">used phones</span>{" "}
+               from anywhere.
+            </h1>
+          </div>
+          <img className="h-96" src={phoneBuy} alt="/" />
+        </div>
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" className="btn btn-sm btn-circle btn-outline">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-sm btn-circle btn-outline">
+            ❯
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        myTheme: {
+          primary: "#264653",
+          secondary: "#2a9d8f",
+          warning: "#e9c46a",
+          accent: "#f4a261",
+          neutral: "#e76f51",
+          "base-100": "#FFFFFF",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
