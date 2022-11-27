6e44e8499854062b5cd8e538f394e57a14d1a77b
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-11-27 14:22:22 +0600

    added env file

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: "AIzaSyDXGUYhTXCPQ7TrZwBHztbL10wTun9Mzcg",
-  authDomain: "oldie-mobile.firebaseapp.com",
-  projectId: "oldie-mobile",
-  storageBucket: "oldie-mobile.appspot.com",
-  messagingSenderId: "632233848650",
-  appId: "1:632233848650:web:5755d363972c78a853dbb3",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig);
